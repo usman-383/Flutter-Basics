e6102815390b88459f37fc95144d30084a39635b
Usman khan <usmankhan.x383@gmail.com>
2026-08-16 06:54:46 +0500

    Update 6 Add Images.dart

---
+++ 6 Add Image.dart
@@ -0,0 +1,10 @@
+/*
+  How to add an image in Flutter?
+
+  1)First add the image in the assets folder of your project.
+  2)Then add the image path in the pubspec.yaml file under the 
+    flutter section.
+  3)Then use the Image widget to display the image in your app.
+  4)The Image widget can display images from various sources such
+   as local files, network URLs, or assets.
+*/
