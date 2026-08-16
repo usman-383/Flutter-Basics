95a15d433b4b08df1c2acf17af9ef1df0d7f8aa2
Usman khan <usmankhan.x383@gmail.com>
2026-08-16 07:58:37 +0500

    Create 8 InkWell Widget.dart

---
+++ 2 Flutter Widgets/8 InkWell Widget.dart
@@ -0,0 +1,6 @@
+/*
+InkWell Widget in Flutter is a material design widget that provides
+a visual feedback when the user taps on it. 
+It is commonly used to create buttons, clickable cards, and other 
+interactive elements in a Flutter app.
+*/
