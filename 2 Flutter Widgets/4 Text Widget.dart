ea7f7f8776590dd5285f946fff2ceb8d77d99e7a
Usman khan <usmankhan.x383@gmail.com>
2026-08-16 06:05:11 +0500

    Create Text Widget

---
+++ 2 Flutter Widgets/4 Text Widget.dart
@@ -0,0 +1,7 @@
+/*
+  Text Widget in Flutter:
+  -> The Text Widget is used to display a string of text with single style. 
+  -> It is one of the most commonly used widgets in Flutter. 
+  -> The Text Widget can be customized with various properties such as
+  font size, color, weight, and more.
+*/
